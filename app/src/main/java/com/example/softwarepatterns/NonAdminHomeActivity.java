aeb8a539c8060418056950a464e1a7fa06272cda
Mark <markgrogan123@gmail.com>
2021-03-24 15:25:04 +0000

    Software Patterns Assignment 4 initial commit of activity classes and corresponding layouts

--- app/src/main/java/com/example/softwarepatterns/NonAdminHomeActivity.java
+++ app/src/main/java/com/example/softwarepatterns/NonAdminHomeActivity.java
@@ -1,14 +1,81 @@
 package com.example.softwarepatterns;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.contentcapture.DataRemovalRequest;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class NonAdminHomeActivity extends AppCompatActivity {
+        RecyclerView rcv3;
+        DatabaseReference userRef;
+        ArrayList<StockItem> stockItems = new ArrayList<>();
+        RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_non_admin_home);
+
+        rcv3 = findViewById(R.id.rcv3);
+        rcv3.setHasFixedSize(true);
+        rcv3.setLayoutManager(new LinearLayoutManager(this));
+        userRef =  FirebaseDatabase.getInstance().getReference("StockItem");
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot s : dataSnapshot.getChildren()) {
+
+                    StockItem stockItem = s.getValue(StockItem.class);
+
+
+                    stockItems.add(stockItem);
+                    adapter = new CustomerAdapter(stockItems);
+                    rcv3.setAdapter(adapter);
+                    adapter.notifyItemInserted(stockItems.size() - 1);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Toast.makeText(NonAdminHomeActivity.this, "Error", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu2, menu);
+        return true;
+
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Intent i = new Intent(NonAdminHomeActivity.this, ViewCustomerActivity.class);
+                startActivity(i);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
